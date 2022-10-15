15c28a8e76f4b91498631a469df715c424d9d4c6
Matt <m.msklee@gmail.com>
2022-10-14 22:35:19 -0400

    Change header icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import Container from "@mui/material/Container";
 import { IconButton, Box, Toolbar, Typography } from "@mui/material";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
 
 export default function Header() {
   return (
-    <Container maxWidth="xl">
-      <Box sx={{ color: "primary.main" }}>
+    <Container maxWidth="xl" sx={{ background: "lightgrey" }}>
+      <Box sx={{ color: "black" }}>
         <Toolbar>
-          <IconButton></IconButton>
-          <Typography sx={{ mr: 2 }}>GARDEN TO-DO</Typography>
-          <Box sx={{ flexGrow: 0 }}>Menu</Box>
+          <IconButton>
+            <FastfoodIcon />
+          </IconButton>
+          <Typography sx={{ mr: 2 }}>TO-DO</Typography>
+          <Box sx={{ flexGrow: 0 }}>{/* <FastfoodIcon /> */}</Box>
         </Toolbar>
       </Box>
     </Container>
